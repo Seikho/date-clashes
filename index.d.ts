ad2e981c96992727da66e7341c7b2bc0751d5f25
Carl Winkler <carl@carlossus.com>
2015-09-25 12:11:12 +0800

    Move project type defs to /index.d.ts for use as npm typings

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+export class Clash {
+    constructor(rangeGetter?: RangeGetter);
+
+    getRange: RangeGetter;
+    flatten(dates: any[], options?: Options): Clashes;
+    getExtremities(dates: any[]): Range;
+    floorDate(date: Date, day?: number): Date;
+    ceilingDate(date: Date, day?: number): Date;
+    isRange(range: Range): boolean;
+}
+
+export interface RangeGetter {
+    (dates: any): Range;
+}
+
+export interface Range {
+    start: Date,
+    end: Date,
+    [key: string]: any;
+}
+
+export interface Clashes {
+    start: Date;
+    end: Date;
+    [index: number]: { date: Date, clashes: Range[] }
+}
+
+export interface Options {
+    startDay?: number;
+    endDay?: number;
+}
+
